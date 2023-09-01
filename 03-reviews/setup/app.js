1b7f3ded01995840ad2a5c93b4a73f36a3635d64
leo <90016459+idkhowisyourday@users.noreply.github.com>
2023-09-01 19:05:45 -0400

    Added profiles for league 4 characters Could not manage to edit psuedo elements and update them real time Finished project3

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -1,84 +1,93 @@
-// local reviews data
-const reviews = [
+// rewrite of the original app.js functionality
+// includes next/back buttons, random profile button
+
+// all profiles data
+const profiles = [
   {
-    id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    img: 'https://www.course-api.com/images/people/person-1.jpeg',
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    "index": 1,
+    "img_src": "https://i.imgur.com/DQwW4fK.png",
+    "name": "graves",
+    "title": "The Outlaw",
+    "info": `Graves is a rugged gunslinger who excels in close-quarters combat. Armed with his trusty shotgun, he deals high burst damage and has the ability to dash and smoke enemies out of their cover.`,
+    // "tier": "S"
   },
   {
-    id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    img: 'https://www.course-api.com/images/people/person-2.jpeg',
-    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    "index": 2,
+    "img_src": "https://i.imgur.com/jpHzeSJ.png",
+    "name": "tryndamere",
+    "title": "The Barbarian King",
+    "info": `Tryndamere is a relentless melee fighter who becomes more powerful as his health decreases. With his undying rage, he can deal devastating critical strikes and becomes immune to death for a short duration.`,
+    // "tier": "A"
   },
   {
-    id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    img: 'https://www.course-api.com/images/people/person-4.jpeg',
-    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    "index": 3,
+    "img_src": "https://i.imgur.com/EAK8AHG.png",
+    "name": "fiora",
+    "title": "The Grand Duelist",
+    "info": `Fiora is a master duelist who excels in one-on-one combat. She possesses incredible speed and precision with her rapier, allowing her to parry enemy attacks and strike vital points for massive damage.`,
+    // "tier": "A"
   },
   {
-    id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
-    img: 'https://www.course-api.com/images/people/person-3.jpeg',
-    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    "index": 4,
+    "img_src": "https://i.imgur.com/uaOFjze.png",
+    "name": "vladimir",
+    "title": "The Crimson Reaper",
+    "info": `Vladimir is a powerful mage who harnesses the dark magic of blood. He heals himself by damaging enemies, becoming increasingly resilient as the battle goes on. Combined with his crowd control abilities, he can turn the tide of team fights.
+`,
+    "tier": "B"
   },
-];
-// select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
+]
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+// id (?), img, name, title, info 
+const img = document.querySelector("#person-img");
+const currName = document.querySelector(".author");
+const title = document.querySelector(".job");
+const info = document.querySelector("#info");
 
-// set starting item
-let currentItem = 0;
+// buttons
+const next_btn = document.querySelector(".next-btn")
+const prev_btn = document.querySelector(".prev-btn")
+const rand_btn = document.querySelector(".rand-btn")
 
-// load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-});
+next_btn.addEventListener('click', nextImg);
+prev_btn.addEventListener('click', prevImg);
+rand_btn.addEventListener('click', randImg);
 
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+let currIndex = 0;
+
+function nextImg() {
+  if (currIndex + 1 >= profiles.length) {
+    currIndex = 0;
+  } else {
+    currIndex += 1;
+  }
+  loadProfile();
 }
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
 
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
+function prevImg() {
+  if (currIndex - 1 < 0) {
+    currIndex = profiles.length - 1
+  } else {
+    currIndex -= 1;
+  }
+  loadProfile();
+}
+
+function randImg() {
+  currIndex = Math.floor(Math.random() * profiles.length);
+  loadProfile();
+}
+
+function loadProfile() {
+  const profile = profiles[currIndex];
+  img.src = profile["img_src"];
+  currName.textContent = profile["currName"];
+  title.textContent = profile["title"];
+  info.textContent = profile["info"];
+  // img.classList.remove('tier-S');
+  // img.classList.remove('tier-A');
+  // img.classList.remove('tier-B');
+  // img.classList.add(`tier-${profile["tier"]}`);
+}
+
+loadProfile();
