17cab52e3278ef68925500312f6174edaae96759
leo <90016459+idkhowisyourday@users.noreply.github.com>
2023-09-05 11:35:28 -0400

    My own theme black white blue? Background: #f1f5f8, Text: #617d98, Title: #102a42, Icons/borders: Black Finished project7 (from scratch)

--- 07-questions/final/app.js
+++ 07-questions/final/app.js
@@ -3,12 +3,13 @@ const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
-  // console.log(btn);
+  console.log(btn);
 
   btn.addEventListener("click", function () {
     // console.log(question);
 
     questions.forEach(function (item) {
+      console.log(item);
       if (item !== question) {
         item.classList.remove("show-text");
       }
--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,16 @@
-//using selectors inside the element
-// traversing the dom
+const questionsList = document.querySelectorAll(".question");
+
+questionsList.forEach((question) => {
+    const btn = question.querySelector(".question-btn");
+
+    btn.addEventListener("click", () => {
+        questionsList.forEach((item) => {
+            // remove selection for all but clicked
+            if (item !== question) {
+                item.classList.remove("question-open");
+            }
+        })
+        // toggles the status of clicked one
+        question.classList.toggle("question-open");
+    })
+})
